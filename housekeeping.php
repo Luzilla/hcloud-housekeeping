db4cdbf1097567ed14287a389ca29f3807ad24d4
till <till@php.net>
2020-07-10 18:24:04 +0200

    Update: display data we are expecting

--- housekeeping.php
+++ housekeeping.php
@@ -8,16 +8,26 @@ $apiKey = getenv('HCLOUD_TOKEN');
 
 echo "Knock, knock!" . PHP_EOL;
 sleep(1);
-echo "This is Hetzner Cloud housekeeping!" . PHP_EOL;
+echo "This is Hetzner Cloud housekeeping!" . PHP_EOL . PHP_EOL;
 
 $hetznerClient = new \LKDev\HetznerCloud\HetznerAPIClient($apiKey);
-foreach ($hetznerClient->servers()->all() as $server) {
+
+$servers = $hetznerClient->servers()->all();
+$volumes = $hetznerClient->volumes()->all();
+$keys    = $hetznerClient->sshkeys()->all();
+
+echo sprintf(
+    "Servers: %d\t\tVolumes: %d\t\tKeys: %d" . PHP_EOL . PHP_EOL,
+    count($servers), count($volumes), count($keys)
+);
+
+foreach ($servers as $server) {
     echo "Deleting server";
     $server->delete();
     echo "   OK" . PHP_EOL;
 }
 
-foreach ($hetznerClient->volumes()->all() as $volume) {
+foreach ($volumes as $volume) {
     echo "Deleting {$volume->name}..." . PHP_EOL;
     if ($volume->server) {
         echo "   Volume is attached to a server: DETACH." . PHP_EOL;
@@ -44,7 +54,7 @@ foreach ($hetznerClient->volumes()->all() as $volume) {
     echo "   OK" . PHP_EOL;
 }
 
-foreach($hetznerClient->sshkeys()->all() as $key) {
+foreach($keys as $key) {
     if (strstr($key->name, 'molecule-generated') !== false) {
         echo "Deleting key {$key->name}: OK" . PHP_EOL;
         $key->delete();
