0655df97b4b0e1d653e3797cb6f5f6789bf6d02b
till <till@php.net>
2020-07-10 18:13:32 +0200

    Fix: handle volumes more efficiently

--- housekeeping.php
+++ housekeeping.php
@@ -1,5 +1,7 @@
 #!/usr/bin/env php
 <?php
+namespace Luzilla\HCloud\Housekeeping;
+
 require 'vendor/autoload.php';
 
 $apiKey = getenv('HCLOUD_TOKEN');
@@ -20,15 +22,30 @@ foreach ($hetznerClient->volumes()->all() as $volume) {
     if ($volume->server) {
         echo "   Volume is attached to a server: DETACH." . PHP_EOL;
         $volume->detach();
-        $hetznerClient->servers()->get($volume->server)->delete();
+        sleep(1);
+
         echo "   Deleting server." . PHP_EOL;
+
+        volume_had_server:
+            try {
+                $server = $hetznerClient->servers()->get($volume->server);
+                $server->delete();
+            } catch (\GuzzleHttp\Exception\ClientException $e) {
+                if ($e->hasResponse()) {
+                    if ($e->getResponse()->getStatusCode() == 423) {
+                        // server is locked
+                        goto volume_had_server;
+                    }
+                }
+            }
     }
+
     $volume->delete();
     echo "   OK" . PHP_EOL;
 }
 
 foreach($hetznerClient->sshkeys()->all() as $key) {
-    if (strstr($key->name, 'molecule-generated') != false) {
+    if (strstr($key->name, 'molecule-generated') !== false) {
         echo "Deleting key {$key->name}: OK" . PHP_EOL;
         $key->delete();
     }
